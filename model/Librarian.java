59bf1587ffc6cfe24a2da2acc74663b12c386e64
argentfd <ermiyassamuel455@gmail.com>
2025-08-01 15:29:32 +0300

    commit

---
+++ model/Librarian.java
@@ -0,0 +1,46 @@
+package model;
+
+import interfaces.Borrowable;
+import interfaces.Returnable;
+
+public class Librarian extends LibraryUser implements Borrowable, Returnable {
+    private String shift;
+
+    public Librarian(String userId, String section, boolean isAuthorized, String shift) {
+        super(userId, section, isAuthorized);
+        this.shift = shift;
+    }
+
+    @Override
+    public void borrowBook() {
+        System.out.println("Librarian processes book for borrowing.");
+    }
+
+    public void borrowBook(String department) {
+        System.out.println("Books borrowed from: " + department + " section.");
+    }
+
+    @Override
+    public String returnBook() {
+        return "Return policy managed by librarian.";
+    }
+
+    public void getShiftDetails() {
+        System.out.println("Shift: " + shift);
+    }
+
+    @Override
+    public void checkCondition() {
+        try {
+            super.checkCondition();
+            System.out.println("Librarian verified.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Librarian ID: " + userId + ", Section: " + section + ", Shift: " + shift);
+    }
+}
