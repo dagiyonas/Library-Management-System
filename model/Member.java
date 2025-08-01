ef020e299e3373191ff54901d9bc6788b39a561f
argentfd <ermiyassamuel455@gmail.com>
2025-08-01 15:36:14 +0300

    member

---
+++ model/Member.java
@@ -0,0 +1,53 @@
+package model;
+
+import interfaces.Borrowable;
+import interfaces.Returnable;
+
+import java.util.ArrayList;
+
+public class Member extends LibraryUser implements Borrowable, Returnable {
+    private ArrayList<String> borrowedBooks;
+
+    public Member(String userId, String section, boolean isAuthorized) {
+        super(userId, section, isAuthorized);
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    @Override
+    public void borrowBook() {
+        System.out.println("Member is borrowing a book.");
+    }
+
+    public void borrowBook(String bookTitle) {
+        borrowedBooks.add(bookTitle);
+        System.out.println("Borrowed: " + bookTitle);
+    }
+
+    @Override
+    public String returnBook() {
+        borrowedBooks.clear();
+        return "Return successful for all borrowed books.";
+    }
+
+    public void listBorrowedBooks() {
+        System.out.println("Borrowed Books:");
+        for (String book : borrowedBooks) {
+            System.out.println("- " + book);
+        }
+    }
+
+    @Override
+    public void checkCondition() {
+        try {
+            super.checkCondition();
+            System.out.println("Member validated.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Member ID: " + userId + ", Section: " + section);
+    }
+}
